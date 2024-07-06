8ad4f37424a9e48c6af7013ace8a312c70910e34
Centy26 <centrineatieno20@gmail.com>
2024-07-06 23:36:10 +0300

    arrow fucntions

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function for square
+  const square = x => x * x;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+
+console.log(divide(2000, 100)); 
+console.log(square(2)); 
+console.log(add(3, 4)); 
